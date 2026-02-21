e143c31eea250e7c5d4a64077ae7d7772d9248aa
shaiag <agamshai@gmail.com>
2026-02-21 20:08:10 +0200

    fix(UI): bundle Google Fonts and Bootstrap Icons locally for Electron offline loading

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -53,10 +53,15 @@ const config = {
       generateBundle(options, bundle) {
         const htmlChunk = bundle['index.html'];
         if (htmlChunk && htmlChunk.type === 'asset' && typeof htmlChunk.source === 'string') {
-          htmlChunk.source = htmlChunk.source.replace('href="./bootstrap.rtl.min.css"', 'href="bootstrap.rtl.min.css"');
+          htmlChunk.source = htmlChunk.source
+            .replace('href="./bootstrap.rtl.min.css"', 'href="bootstrap.rtl.min.css"')
+            .replace('href="./bootstrap-icons.min.css"', 'href="bootstrap-icons.min.css"')
+            .replace('href="./fonts/outfit.css"', 'href="fonts/outfit.css"')
+            .replace(/src="\.\//g, 'src="')
+            .replace(/href="\.\//g, 'href="');
         }
-      },
-    },
+      }
+    }
   ],
 };
 
